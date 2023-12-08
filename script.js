96d8e27e594b0899e293899d78fb036b2e9737ba
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-08 14:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 const Btn = document.querySelector("#enterBtn");
 const P = document.querySelector("#status");
 Btn.addEventListener("click",function(){
-	P.remove();
-	let H1 = document.createElement("h1");
-	H1.innerText = "Entered Metaverse";
-	body.appendChild(H1);
+	P.innerText = "Entered Metaverse"
 });
 
